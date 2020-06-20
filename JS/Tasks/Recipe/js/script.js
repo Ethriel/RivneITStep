53a7f1d1b1138fbfc7e63171d117aa966bde42ac
Kerfoer <bartolomio177@gmail.com>
2020-06-20 18:40:45 +0300

    recipe task is done

--- JS/Tasks/Recipe/js/script.js
+++ JS/Tasks/Recipe/js/script.js
@@ -1,18 +1,112 @@
+let diffIcons = ["fa-battery-full", "fa-battery-half", "fa-battery-quarter"];
+let images = ["../img/salad.png", "../img/cake.png", "../img/hotMeal.jpg"];
+
 function addRecipe() {
+
+    let allValues = [];
+
+    pushValueBySelectorToArray("#recipeName", allValues);
+    pushValueBySelectorToArray("#recipeDescription", allValues);
+    pushValueBySelectorToArray("select", allValues);
+    allValues.push(getDiffValue());
+    pushValueBySelectorToArray("#nOfServings", allValues);
+    pushValueBySelectorToArray("#prepTime", allValues);
+    pushValueBySelectorToArray("#cookTime", allValues);
+    pushValueBySelectorToArray("#ingridientsArea", allValues);
+
+    for (let i = 0; i < allValues.length; i++) {
+        if (isStringEmptyOrNull(allValues[i])) {
+            alert("Some fields are empty. Fill them, please");
+            return;
+        }
+    }
+
+    // recipe obj
+    let recipe = {
+        rName: allValues[0],
+        rDescription: allValues[1],
+        rType: allValues[2],
+        rDifficulty: allValues[3],
+        rNOfServings: allValues[4],
+        rPreparationTime: allValues[5],
+        rCookingTime: allValues[6],
+        rIngridients: allValues[7]
+    };
+
+    recipeDone(recipe);
+}
+
+// compose recipe info div
+function recipeDone(recipe) {
     let recDiv = document.querySelector(".recipe-container");
-    let allStrs = [];
-    let recNameInput = document.querySelector("#recipeName");
-    let recNameValue = recNameInput.value;
-    allStrs.push(recNameValue);
+    let recDoneDiv = document.querySelector(".recipe-done");
 
-    let recDescrArea = document.querySelector("#recipeDescription");
-    let recDescrValue = recDescrArea.value;
-    allStrs.push(recDescrValue);
+    selectImage(recipe.rType);
+    fillElementInnerValue(".r-done-name", recipe.rName);
+    fillElementInnerValue(".r-done-type", recipe.rType);
+    fillDiffLevel(recipe);
+    fillElementInnerValue(".d-1", recipe.rDescription);
+    fillElementInnerValue(".d-2", `<b>Ingridients: </b> ${recipe.rIngridients}`, true);
+    fillElementInnerValue("#srv", recipe.rNOfServings);
+    fillElementInnerValue("#pTime", recipe.rPreparationTime);
+    fillElementInnerValue("#cTime", recipe.rCookingTime);
 
-    let recTypeSelect = document.querySelector("select");
-    let rectTypeValue = recTypeSelect.value;
-    allStrs.push(rectTypeValue);
+    recDoneDiv.style.display = "flex";
+    recDiv.style.display = "none";
+};
 
+// fill difficulty level
+function fillDiffLevel(recipe) {
+    let recDiffLevel = document.querySelector(".diff-level");
+    recDiffLevel.classList.add("fas");
+    recDiffLevel.classList.add(getDiffIcon(recipe.rDifficulty));
+    fillElementInnerValue("#diff", getDiffText(recipe.rDifficulty));
+};
+
+// fill innerText or innerHTML of an element chosen by selector with given value
+function fillElementInnerValue(selector, value, innerHtml = false) {
+    let element = document.querySelector(selector);
+    if (innerHtml) {
+        element.innerHTML = value;
+    } else {
+        element.innerText = value;
+    }
+};
+
+// select an image by given meal type
+function selectImage(meal) {
+    let recImg = document.querySelector(".r-img");
+    let imgSrc = "";
+    switch (meal) {
+        case "Salad":
+            imgSrc = images[0];
+            break;
+        case "Cake":
+            imgSrc = images[1];
+            break;
+        case "Hot meal":
+            imgSrc = images[2];
+            break;
+    }
+    recImg.src = imgSrc;
+};
+
+// get value of an element chosen by selector
+function getValueOfElement(selector) {
+    let element = document.querySelector(selector);
+    let val = element.value;
+    return val;
+};
+
+// push a value of an element chosen by selector to given array
+function pushValueBySelectorToArray(selector, arr) {
+    let value = getValueOfElement(selector);
+    arr.push(value);
+    console.log(value);
+};
+
+// get difficulty level
+function getDiffValue() {
     let allRb = Array.from(document.querySelectorAll("input[type='radio']"));
     let recDifficultyValue = "";
     for (let i = 0; allRb.length; i++) {
@@ -21,56 +115,46 @@ function addRecipe() {
             break;
         }
     }
-    allStrs.push(recDifficultyValue);
+    return recDifficultyValue;
+};
 
-    let recNOfServings = document.querySelector("#nOfServings");
-    let recNOfServingsValue = recNOfServings.value;
-    allStrs.push(recNOfServingsValue);
-
-    let recPrepTime = document.querySelector("#prepTime");
-    let recPrepTimeValue = recPrepTime.value;
-    allStrs.push(recPrepTimeValue);
-
-    let recCookingTime = document.querySelector("#cookTime");
-    let recCookingTimeValue = recCookingTime.value;
-    allStrs.push(recCookingTimeValue);
-
-    let recIngridients = document.querySelector("#ingridientsArea");
-    let recIngridientsValue = recIngridients.value;
-    allStrs.push(recIngridientsValue);
-
-    for (let i = 0; i < allStrs.length; i++) {
-        if (isStringEmptyOrNull(allStrs[i])) {
-            alert("Some fields are empty. Fill them, please");
-            return;
-        }
-    }
-
-    let recipe = {
-        rName: recNameValue,
-        rDescription: recDescrValue,
-        rType: rectTypeValue,
-        rDifficulty: recDifficultyValue,
-        rNOfServings: recNOfServingsValue,
-        rPreparationTime: recPrepTimeValue,
-        rCookingTime: recCookingTimeValue,
-        rIngridients: recIngridientsValue
-    };
-
-    let recDoneDiv = document.querySelector(".recipe-done");
-    let recImg = document.querySelector(".r-img");
-    let recDoneName = document.querySelector(".r-done-name").innerText = recipe.rName;
-    let recDoneType = document.querySelector(".r-done-type").innerText = recipe.rType;
-    let recDiffLevel = document.querySelector(".diff-level");
-    let recDescr = document.querySelector(".d-1").innerText = recipe.rDescription;
-    let recIngridientsText = document.querySelector(".d-2").innerText = `<b>Ingridients: </b> ${recipe.rIngridients}`;
-
-    console.log(recipe);
-
-    recDoneDiv.style.display = "flex";
-    recDiv.style.display = "none";
-}
-
+// check if string null or empty
 function isStringEmptyOrNull(str) {
     return str == "" || str == null;
+};
+
+// get icon of difficulty level
+function getDiffIcon(diff) {
+    let d = "";
+    console.log(diff);
+
+    switch (diff) {
+        case "hard":
+            d = diffIcons[0];
+            break;
+        case "moderate":
+            d = diffIcons[1];
+            break;
+        case "easy":
+            d = diffIcons[2];
+            break;
+    }
+    return d;
+};
+
+// get diffuclty text
+function getDiffText(diff) {
+    let d = "";
+    switch (diff) {
+        case "hard":
+            d = " Hard level";
+            break;
+        case "moderate":
+            d = " Moderate level";
+            break;
+        case "easy":
+            d = " Easy level";
+            break;
+    }
+    return d;
 }
